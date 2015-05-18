8b1e162576b84adc04ab0203f57b281642109455
Nazariy <nazariy.papizh@gmail.com>
2015-05-18 17:20:34 +0300

    working with omniauth v#5

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,13 +40,13 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.find_for_facebook_oauth access_token
-    if user = User.where(:url => access_token.info.urls.Facebook).first
-      user
-    else
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Facebook, :username => access_token.extra.raw_info.name, :nickname => access_token.extra.raw_info.username, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20])
-    end
-  end
+  # def self.find_for_facebook_oauth access_token
+  #   if user = User.where(:url => access_token.info.urls.Facebook).first
+  #     user
+  #   else
+  #     User.create!(:provider => access_token.provider, :url => access_token.info.urls.Facebook, :username => access_token.extra.raw_info.name, :nickname => access_token.extra.raw_info.username, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20])
+  #   end
+  # end
   def self.find_for_vkontakte_oauth access_token
      if user = User.where(:url => access_token.info.urls.Vkontakte).first
        user
