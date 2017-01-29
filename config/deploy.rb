60e8beae76c438487a3bcd8577dd472082da3664
Sasha Stadnik <stadniklksndr@gmail.com>
2017-01-29 21:57:00 +0200

    Update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+#
+namespace :deploy do
+  before 'deploy:migrate', 'deploy:db:create'
+end
