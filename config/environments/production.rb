dcb3a7b15c471f1dbee7773b56b3c62521e2d4d5
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-07 03:27:32 +0200

    Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
