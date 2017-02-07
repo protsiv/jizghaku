d89eca39331f6cf39b1da09a63bb5c65c61b5af3
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-07 03:54:38 +0200

    Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.log_level = :info
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
