98745a657d68941a4662b81b4d1244da317c6b17
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-11 03:33:35 +0200

    Warning changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,40 +1,50 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.serve_static_files = true
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  #config.assets.debug = true
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  #config.assets.css_compressor = :sass
+
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.  :info, :debug
+  config.log_level = :info
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+  #config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -47,12 +57,14 @@ Rails.application.configure do
   # replace this with your development tracker code
   GA.tracker = "UA-63240701-1"
 
-  # connection file
-  # APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
 
-  #Send Email In Development (Use Gmail's Servers)
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'yizhaky.com.ua' }
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
@@ -63,8 +75,5 @@ Rails.application.configure do
       password:  "pasha1991",
       :authentication       => :plain,
       :enable_starttls_auto => true
-  }
-  config.action_mailer.default_url_options = {
-      :host => "localhost:3000"
   }
 end
