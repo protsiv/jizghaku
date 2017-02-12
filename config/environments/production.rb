cd4987dabe61b2c6f704575003439c5cb570f256
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-12 03:09:45 +0200

    Update nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.serve_static_files = true
-  config.static_cache_control = 'public, max-age=1000'
+  #config.serve_static_files = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
