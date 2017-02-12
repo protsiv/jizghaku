e9e96c3a48a8ac1fa99bed3d6c2ee04ab7d12b42
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-12 03:03:03 +0200

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.serve_static_files = true
+  config.static_cache_control = 'public, max-age=1000'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
