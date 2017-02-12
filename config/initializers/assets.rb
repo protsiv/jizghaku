161486bbc1a6eecc176804bb58939cc400cb19cb
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-12 21:50:21 +0200

    Up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile << Proc.new { |path, fn| fn =~ /vendor\/assets\/images/ }
+Rails.application.config.assets.precompile << Proc.new { |path, fn| fn =~ /vendor\/assets\/woff/ }
 #Rails.application.config.assets.precompile << Proc.new { |path, fn| fn =~ /vendor\/assets/ && !%w(.js .css).include?(File.extname(path)) }
