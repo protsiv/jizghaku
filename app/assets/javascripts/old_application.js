1939628ecf2004d97721bb8e3e9e9c09ca65b37c
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-12 02:01:42 +0200

    Update js

--- app/assets/javascripts/old_application.js
+++ app/assets/javascripts/old_application.js
@@ -12,3 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
+
+//= require parallax
+//= require jquery.parallax
+
+//= require owl.carousel
+//= require mixitup
+//= require bxslider
+//= require_tree .
+//= require ckeditor/init
+//= require jquery.slick
+
+$(function(){ $(document).foundation(); });
