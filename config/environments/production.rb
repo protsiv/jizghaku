c3f925a779a29b745426f0031f6a8a6af456a79f
Sasha Stadnik <stadniklksndr@gmail.com>
2017-02-25 03:41:06 +0200

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
